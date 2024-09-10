f443f20c5f6dd81b4c2a18deee815e1a94ff953e
Armagan Ercan <armagan.erc@gmail.com>
2024-09-10 10:35:40 -0400

    Initial Commit

---
+++ Pokemon Identifier/Pokemon_IdentifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pokemon_IdentifierApp.swift
+//  Pokemon Identifier
+//
+//  Created by Armagan Ercan on 2024-09-10.
+//
+
+import SwiftUI
+
+@main
+struct Pokemon_IdentifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
